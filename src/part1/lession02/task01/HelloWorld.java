c75f55b93571a8b21c421c221ce3a49984812677
Airat Khafizov <airat85@bk.ru>
2019-10-31 07:09:15 +0300

    Initial Commit from IntelliJ Idea

---
+++ src/part1/lession02/task01/HelloWorld.java
@@ -0,0 +1,31 @@
+package part1.lession02.task01;
+
+/**
+ * Задание 1. Написать программу ”Hello, World!”. В ходе выполнения программы она должна выбросить исключение и завершиться с ошибкой.
+ *
+ * Смоделировав ошибку «NullPointerException»
+ * Смоделировав ошибку «ArrayIndexOutOfBoundsException»
+ * Вызвав свой вариант ошибки через оператор throw
+ */
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+
+        //«NullPointerException»
+        try {
+            String s1 = null;
+            System.out.println(s1.substring(0));
+        } catch(NullPointerException ex) {
+            ex.printStackTrace();
+        }
+        // «ArrayIndexOutOfBoundsException»
+        try {
+            int[] s2 = {1, 2, 3, 4, 5};
+            System.out.println(s2[5]);
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            ex.printStackTrace();
+        }
+        //MyException
+        throw new Exception("My custom Exception");
+
+    }
+}

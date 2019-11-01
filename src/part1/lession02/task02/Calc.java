a8f39fa928d161a0568e30275325b29b544b10ea
Airat Khafizov <airat85@bk.ru>
2019-11-01 13:16:10 +0300

    Добавил выдачу и отрицательных числе (требование преподавателя в чате)

--- src/part1/lession02/task02/Calc.java
+++ src/part1/lession02/task02/Calc.java
@@ -10,33 +10,66 @@ import java.util.Random;
  * в этом случае генерировать исключение.
  */
 public class Calc {
-    //private static final int MAX_VALUE = 10;
+    private static final int MAX_VALUE = 10;
     final Random random = new Random();
     /**
      * @param n количество случайных чисел
+     * @return Массив из двух элементво. 0 -  {@link List} Содержит числа удволетворяющие услвоию, 1 - {@link List} отрицательные числа
      */
-    public void calculate(int n) throws Exception {
-        if(n < 1) {
+    public List[] calculate(int n) throws Exception {
+        List<Integer> res = new ArrayList<Integer>();
+        List<Integer> wrongNumbers = new ArrayList<Integer>();
+        if (n < 1) {
             throw new Exception("Для генерации параметр n должен быть больше 0");
         }
         List<Integer> numbers = new ArrayList<Integer>();
-        for(int i=0; i<n; i++)
-            numbers.add(random.nextInt());
+        for (int i = 0; i < n; i++)
+            //numbers.add(random.nextInt(MAX_VALUE));
+            numbers.add(random.nextInt(MAX_VALUE));
 
-        for (Integer k  :numbers) {
-            if(k < 0) {
-                throw new Exception("Найдено отрицательное число");
-            }
-            Double q = Math.sqrt(k);
-
-            if(Math.pow(q.intValue(),2) == k) {
-                System.out.println(k);
+        for (Integer k : numbers) {
+            try {
+                if(this.isSatisfy(k))
+                    res.add(k);
+            } catch (Exception ex) {
+                wrongNumbers.add(k);
             }
         }
+
+        return new List[]{res, wrongNumbers};
     }
+
+    /**
+     * Проверка удовлетворяет ли число условию
+     * @param k число для анализа
+     * @return  true|false удовлетворяет|неудовлетворяет
+     * @throws Exception
+     */
+    private boolean isSatisfy(int k) throws Exception {
+        if (k < 0) {
+            throw new Exception("Найдено отрицательное число");
+        }
+        Double q = Math.sqrt(k);
+
+        if (Math.pow(q.intValue(), 2) == k) {
+            return true;
+        }
+        return false;
+    }
+
 
     public static void main(String[] args) throws Exception {
         Calc c = new Calc();
-        c.calculate(10);
+        List[] res = c.calculate(10000);
+        if(res[0] != null && res[0].size() > 0) {
+            res[0].forEach(item -> System.out.println(item));
+        } else {
+            System.out.println("Чисел удовлетворяющих критерию не найдено");
+        }
+
+        if(res[1] != null && res[1].size()  > 0) {
+            System.out.println("Найденные отрицательные числа:");
+            res[1].forEach(item -> System.out.println(item));
+        }
     }
 }

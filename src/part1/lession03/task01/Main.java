48edc04b4bcfcd8ce6f6693e49e54b8eb0bced51
Airat Khafizov <airat85@bk.ru>
2019-11-05 12:56:17 +0300

    - мертвый код

--- src/part1/lession03/task01/Main.java
+++ src/part1/lession03/task01/Main.java
@@ -3,10 +3,7 @@ package part1.lession03.task01;
 import helpers.Helper;
 
 public class Main {
-    private static final int MAX_VALUE = 100;
-    private static final int MIN_VALUE = -100;
     public static void main(String[] args) throws Exception {
-        //MathBox mb = new MathBox(Helper.getArrayOfRandomInts(10,MIN_VALUE,MAX_VALUE));
         Number[] data = {30,20,30,40,50,71,80,90,91, null};
         MathBox mb = new MathBox(data);
         Number[] data2 = {20,20,30,50,40,71,71,80,90,91, null};

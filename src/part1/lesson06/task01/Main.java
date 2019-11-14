13c611e409db5436fcb819d04b3d18de3b59921d
Airat Khafizov <airat85@bk.ru>
2019-11-14 14:19:08 +0300

    ДЗ6 для PR

---
+++ src/part1/lesson06/task01/Main.java
@@ -0,0 +1,77 @@
+package part1.lesson06.task01;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * Задание 1. Написать программу, читающую текстовый файл. Программа должна составлять отсортированный по алфавиту список слов, найденных в файле и сохранять его в файл-результат.
+ * Найденные слова не должны повторяться, регистр не должен учитываться. Одно слово в разных падежах – это разные слова.
+ * @author KhafizovAR on 12.11.2019.
+ * @project Stc20JavaMiddle
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        WordCounter.run("files/source.txt", "files/words.txt");
+    }
+
+    /**
+     * Класс извлечения слов
+     */
+    static class WordCounter {
+        /**
+         * Запуск процесса извлечения слов
+         * @param fileName          наименование/путь файла исходника
+         * @param outputFileName    наименование/путь результирующего файла со списком найденных слов
+         */
+        public static void run(String fileName, String outputFileName) {
+            List<String> words = readFile(fileName);
+            Collections.sort(words);
+            writeToFile(words, outputFileName);
+        }
+
+        /**
+         * Метод чтения файлов
+         * @param fileName  наименование файла
+         * @return
+         */
+        private static List<String> readFile(String fileName) {
+            List<String> words = new ArrayList<>();
+            try(FileReader fr = new FileReader(fileName)) {
+                BufferedReader br = new BufferedReader(fr);
+                String s;
+                while ((s = br.readLine()) != null)
+                    for (String substr : s.split("[\\p{Punct}\\s]+")) {
+                        if(!words.contains(substr.toLowerCase())) {
+                            words.add(substr.toLowerCase());
+                        }
+                    }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return words;
+        }
+
+        /**
+         * Метод записи реузльтата
+         * @param words     список слов для записи
+         * @param fileName  наименование выходного файла
+         */
+        private static void writeToFile(List<String> words, String fileName) {
+            try(FileWriter fw = new FileWriter(fileName)) {
+                fw.write("-------------" + new Date().toString() + "-------------\n");
+                for ( String s: words ) {
+                    fw.write(s + "\n");
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}

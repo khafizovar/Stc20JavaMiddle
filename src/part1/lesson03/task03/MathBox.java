28c98f329e9b91794070f72a7283d7693e176d2d
Airat Khafizov <airat85@bk.ru>
2019-12-02 06:57:31 +0300

    Добавил типизацию

--- src/part1/lesson03/task03/MathBox.java
+++ src/part1/lesson03/task03/MathBox.java
@@ -7,10 +7,10 @@ import java.util.Set;
 /**
  * @author KhafizovAR
  */
-public class MathBox extends ObjectBox {
+public class MathBox extends ObjectBox<Number> {
 
     public MathBox(Number[] number) {
-        objectCollection = new HashSet<>(Arrays.asList(number));
+        objectCollection = new HashSet<Number>(Arrays.asList(number));
     }
 
     /**
@@ -19,9 +19,9 @@ public class MathBox extends ObjectBox {
      */
     public Number summator() {
         double sum = 0;
-        for (Object n: this.objectCollection) {
+        for (Number n: this.objectCollection) {
             if(n != null)
-                sum += ((Number)n).doubleValue();
+                sum += n.doubleValue();
         }
         return sum;
     }
@@ -31,7 +31,7 @@ public class MathBox extends ObjectBox {
      * @param num делитель
      */
     public void splitter(double num) {
-        Set<Object> rez = new HashSet<>();
+        Set<Number> rez = new HashSet<>();
         if(this.objectCollection.contains(null)) {
             rez.add(null);
             this.objectCollection.remove(null);
@@ -49,49 +49,6 @@ public class MathBox extends ObjectBox {
      */
     public boolean removeInt(Integer num) {
         return this.objectCollection.remove(num);
-    }
-
-    /**
-     * Добавление объекта в коллекцию
-     * @param obj  Добавляемый объект
-     */
-    @Override
-    public void addBox(Object obj) {
-        if(obj instanceof Number) {
-            this.objectCollection.add((Number)obj);
-        } else {
-            throw new IllegalArgumentException("obj must be instance of Number");
-        }
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((objectCollection == null) ? 0 : objectCollection.hashCode());
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "MathBox [objectCollection=" + objectCollection + "]";
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        MathBox other = (MathBox) obj;
-        if (objectCollection == null) {
-            if (other.objectCollection != null)
-                return false;
-        } else if (!objectCollection.equals(other.objectCollection))
-            return false;
-        return true;
     }
 
 }
--- src/part1/lesson03/task03/ObjectBox.java
+++ src/part1/lesson03/task03/ObjectBox.java
@@ -6,14 +6,14 @@ import java.util.Set;
 /**
  * @author KhafizovAR
  */
-public class ObjectBox {
-    Set<Object> objectCollection = new HashSet<>();
+public class ObjectBox<T> {
+    Set<T> objectCollection = new HashSet<>();
 
     /**
      * Добавление объекта в коллекцию
      * @param obj  Добавляемый объект
      */
-    public void addBox(Object obj) {
+    public void addBox(T obj) {
         this.objectCollection.add(obj);
     }
 
@@ -21,7 +21,7 @@ public class ObjectBox {
      * Удаление объекта из коллекции
      * @param obj   удаляемый объект
      */
-    public void deleteObject(Object obj) {
+    public void deleteObject(T obj) {
         this.objectCollection.remove(obj);
     }
 
@@ -31,31 +31,6 @@ public class ObjectBox {
      */
     public String dump() {
         return this.toString();
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((objectCollection == null) ? 0 : objectCollection.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        ObjectBox other = (ObjectBox) obj;
-        if (objectCollection == null) {
-            if (other.objectCollection != null)
-                return false;
-        } else if (!objectCollection.equals(other.objectCollection))
-            return false;
-        return true;
     }
 
     @Override
